398df08dfefe254dc3fe3a30490857f34f4fcaf2
pandrzejak <piotr.andrzejak@primaris.eu>
2020-11-16 17:32:23 +0100

    init

---
+++ src/test/java/guru/springframework/springmvn/SpringMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
